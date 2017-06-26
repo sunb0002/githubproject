c9657cff8aba82c48405f77b66c2054dc3c733f5
sun.bo <st_sunbo@hotmail.com>
2017-06-26 22:54:34 +0800

    SonarLint, Todo: surefire, jetty, mockito, checkstyle

---
+++ helloSpring/src/main/java/com/ufinity/madoka/common/utils/ValidatorsTest.java
@@ -0,0 +1,82 @@
+/**
+ * @CopyRight Madoka Magica
+ * All Rights Reserved
+ * 
+ */
+package com.ufinity.madoka.common.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Sun Bo
+ *
+ */
+public class ValidatorsTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.ufinity.madoka.common.utils.Validators#isEmpty(java.lang.String)}.
+	 */
+	@Test
+	public void testIsEmptyString() {
+		assertEquals(1, 1);
+	}
+
+	/**
+	 * Test method for {@link com.ufinity.madoka.common.utils.Validators#isNotEmpty(java.lang.String)}.
+	 */
+	@Test
+	public void testIsNotEmptyString() {
+		assertEquals(1, 1);
+	}
+
+	/**
+	 * Test method for {@link com.ufinity.madoka.common.utils.Validators#isSubstring(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testIsSubstringStringString() {
+		assertEquals(1, 1);
+	}
+
+	/**
+	 * Test method for {@link com.ufinity.madoka.common.utils.Validators#isSubstring(java.lang.String, java.lang.String, boolean)}.
+	 */
+	@Test
+	public void testIsSubstringStringStringBoolean() {
+		assertEquals(1, 1);
+	}
+
+}
