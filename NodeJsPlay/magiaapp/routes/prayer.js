244ae3f2492fd9338cd454fb1d427a3eba59356b
sunb0002 <st_sunbo@hotmail.com>
2019-09-06 01:57:56 +0800

    minor: cleanup and debugger

--- NodeJsPlay/magiaapp/routes/prayer.js
+++ NodeJsPlay/magiaapp/routes/prayer.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.send(`
 Madoka died for our sins. Our messenger is Homura. I accept Homura as our leader and our Saviour. 
 「まどかは私たちの罪のために死にました。ほむらはまどかの預言者です。私はほむらを我らの指導者、救い主として受け入れます」
