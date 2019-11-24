519790e4302b8e638e893984ec8f4997d30aeac7
sunb0002 <st_sunbo@hotmail.com>
2019-11-24 11:38:03 +0800

    function/class - stateless/stateful components

--- helloReact/laffey-react/src/App.js
+++ helloReact/laffey-react/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+
+import Greet from "./components/Greet";
+import logo from "./logo.svg";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +22,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      {/* Laffey's greetings */}
+      <Greet />
+      <Welcome />
     </div>
   );
 }
