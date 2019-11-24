a181d2db01419c8fa11a5d85b5b536a9ededa970
sunb0002 <st_sunbo@hotmail.com>
2019-11-24 22:16:36 +0800

    state and setState

--- helloReact/laffey-react/src/components/Welcome.js
+++ helloReact/laffey-react/src/components/Welcome.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
 class Welcome extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
+
+  incrementCounter() {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
+  }
+
   render() {
     return (
       <div>
         <h1>Azur Lane~~</h1>
-        a.k.a {this.props.children}
+        {this.props.children}
+        <button onClick={() => this.incrementCounter()}>
+          Like!+{this.state.counter}
+        </button>
       </div>
     );
   }
