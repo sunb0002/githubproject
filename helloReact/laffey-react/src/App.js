b82ff5c975b9f99ee986633bca4e7be8c0d9ef0f
sunb0002 <st_sunbo@hotmail.com>
2019-12-02 17:10:36 +0800

    Basic form handling

--- helloReact/laffey-react/src/App.js
+++ helloReact/laffey-react/src/App.js
@@ -1,27 +1,34 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
+import React from "react";
 
-import Greet from './components/Greet';
-import HMSNameList from './components/HMSNameList';
-import Stylesheet from './components/stylesheet/Stylesheet';
-import Welcome from './components/Welcome';
+import Form from "./components/form/Form";
+import Greet from "./components/Greet";
+import HMSNameList from "./components/HMSNameList";
+import Stylesheet from "./components/stylesheet/Stylesheet";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       {/* Laffey's greetings */}
       <Welcome>#azurlane_anime #azurlane_official</Welcome>
-      {/* The Starter fleet */}
       <hr />
+
+      {/* The Starter fleet */}
       <Greet name="Laffey" type="DD" />
       <Greet name="Javelin" type="DD" />
       <Greet name="Z23" type="DD" />
       <hr />
+
       {/* The Royal Navy */}
       <HMSNameList />
       <hr />
+
       <Stylesheet />
+      <hr />
+
+      <Form />
       <hr />
     </div>
   );
---
+++ helloReact/laffey-react/src/components/form/Form.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      taskname: "oil excavation",
+      reward: "oil"
+    };
+
+    // Preserve the inital state
+    this.baseState = this.state;
+  }
+
+  handleNameChange = event => {
+    this.setState({
+      taskname: event.target.value
+    });
+  };
+
+  handleRewardChange = event => {
+    this.setState({
+      reward: event.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    window.confirm(
+      `Commission started~ ${this.state.taskname}=>${this.state.reward}`
+    );
+  };
+
+  handleReset = () => {
+    this.setState(this.baseState);
+  };
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label>Commission: </label>
+          <input
+            type="text"
+            value={this.state.taskname}
+            onChange={this.handleNameChange}
+          />
+        </div>
+        <div>
+          <label>Reward: </label>
+          <select value={this.state.reward} onChange={this.handleRewardChange}>
+            <option value="oil">OIL</option>
+            <option value="gold">GOLD</option>
+            <option value="loli">LOLI</option>
+          </select>
+        </div>
+        {/* 也可以用<form onSubmit={xxx}>, 但是xxx的最后要放event.preventDefault()防止刷新页面 */}
+        <button type="button" onClick={this.handleSubmit}>
+          Submit
+        </button>
+        <button type="reset" onClick={this.handleReset}>
+          Reset
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
