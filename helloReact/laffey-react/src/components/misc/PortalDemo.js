ebd7df8626ffe138fb47e81e8ddc06c71be4c2ca
sunb0002 <st_sunbo@hotmail.com>
2019-12-20 00:01:51 +0800

    import image asset

--- helloReact/laffey-react/src/components/misc/PortalDemo.js
+++ helloReact/laffey-react/src/components/misc/PortalDemo.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import belfast from './assets/belfast.gif';
+
+// Nice to put assets in each module
+// If to use global absolute path for import, neet to tweek config:
+// https://create-react-app.dev/docs/importing-a-component/#absolute-imports
 function PortalDemo() {
   const portalRoot = document.getElementById("portal-modal-root");
 
-  return ReactDOM.createPortal(<span>bulin~~</span>, portalRoot);
+  return ReactDOM.createPortal(
+    <img alt="bulin belfast~" src={belfast} />,
+    portalRoot
+  );
 }
 
 export default PortalDemo;
