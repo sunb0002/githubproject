eb03fe3793c9157738ba0326502a586d9d891d0f
sunb0002 <st_sunbo@hotmail.com>
2020-01-06 17:50:46 +0800

    minor text change

--- helloReact/laffey-react/src/components/stylesheet/Stylesheet.js
+++ helloReact/laffey-react/src/components/stylesheet/Stylesheet.js
@@ -1,13 +1,15 @@
-import React from "react";
-import "./appStyles.css";
-import styles from "./appStyles.module.css";
+import './appStyles.css';
+
+import React from 'react';
+
+import styles from './appStyles.module.css';
 
 function Stylesheet() {
   return (
     <div>
-      <h2 className="error">Error</h2>
+      <h2 className="error">Hard Mode</h2>
       {/* In React, xxx.module.css will become localized/scoped css after compiling */}
-      <h2 className={styles.success}>Success</h2>
+      <h2 className={styles.success}>Easy Mode</h2>
     </div>
   );
 }
