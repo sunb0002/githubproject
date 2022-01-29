e84a025d335c54b7cc0504c0d277bc707fd595c7
sunb0002 <sunb0002@gmail.com>
2022-01-29 15:27:15 -0700

    mui play

---
+++ helloReact/bulin-rtk-mui/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import { Mui1 } from "./components/misc/Mui1";
+
+function App() {
+    return (
+        <div className="App">
+            <Mui1></Mui1>
+            <hr />
+        </div>
+    );
+}
+
+export default App;
---
+++ helloReact/bulin-rtk-mui/src/components/misc/Mui1.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+import React from "react";
+
+export const Mui1 = () => {
+    return (
+        <div>
+            <Button variant="contained" color="success">
+                Hello Bulin
+            </Button>
+        </div>
+    );
+};
