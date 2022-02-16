503d43afb032b4e662cb1179b20786b9bc7feab3
sunb0002 <sunb0002@gmail.com>
2022-02-16 11:27:43 -0700

    autoplay

---
+++ helloReact/bulin-mui-gql/src/stories/TaskPlay.stories.jsx
@@ -0,0 +1,43 @@
+import { screen, userEvent } from "@storybook/testing-library";
+import React from "react";
+import { useState } from "react";
+
+import Task from "./Task";
+
+export default {
+    component: Task,
+    title: "Example/TaskPlay",
+    argTypes: {
+        btnBg: {
+            options: ["red", "green", "blue"],
+            control: { type: "radio" },
+        },
+    },
+    parameters: {
+        actions: {
+            handles: ["click button"],
+        },
+    },
+};
+
+const buildTemplate = (state) => (argTypes) => {
+    const [title, setTitle] = useState("Play Title");
+    const props = {
+        task: {
+            state,
+            title,
+        },
+        setTitle,
+        btnBg: argTypes.btnBg,
+    };
+    return <Task {...props} />;
+};
+
+export const Pinned = buildTemplate("TASK_PINNED");
+Pinned.play = async () => {
+    const inputElm = screen.getByRole("textbox");
+    userEvent.clear(inputElm);
+    await userEvent.type(inputElm, "New Title Lah", { delay: 300 });
+    const buttonElm = screen.getByRole("button");
+    userEvent.click(buttonElm);
+};
