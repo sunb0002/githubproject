9c266c2ac6586e76ae047b8a3f2a8ef67b7b0285
Bo Sun <sunbo@microsoft.com>
2024-12-20 16:07:37 -0800

    Websocket basic

--- helloPython/flask-play/app.py
+++ helloPython/flask-play/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send
 from markupsafe import escape
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "to encrypt conversations"
+socketio = SocketIO(app)
+if __name__ == '__main__':
+    socketio.run(app)
+
+##### Flask General #####
 
 
 @app.route('/', methods=['GET', 'POST'])
 def route_basic():
     return {"output": "Hello World!"}
+
+
+@app.route('/favicon.ico')
+def ignore_favicon():
+    return '', 204
 
 
 @app.route('/<name>/<int:age>')
@@ -23,3 +36,20 @@ def route_jinja2_template(name=None):
 @app.errorhandler(Exception)
 def route_error(e):
     return f"Something is wrong! Details:<br/><br/>{e}", 503
+
+
+##### WebSocket #####
+
+@app.route('/ws')
+def route_websocket():
+    return render_template('ws.html', user="madoka")
+
+
+@socketio.on('join')
+def handle_my_custom_event(json):
+    print(f'Someone joined us: {json}')
+    # Send unnamed message
+    send(f"You said: {json}", broadcast=False)
+    # Emit named message
+    emit('server event', f"Named event - you said: {json}")
+    return
